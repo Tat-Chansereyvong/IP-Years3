e262d4e677f37f7174b58866d182d12973cdac96
Tat Chansereyvong <sereyvongtat@gmail.com>
2025-11-25 10:48:46 +0700

    Add router

---
+++ Practice/E-Commerce/src/components/SearchBoxComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="search-box-component">
+        <input
+        type="text"
+        class="search-input"
+        :placeholder="placeholder"
+        v-model="searchQuery"
+        @input="handleInput"
+        />
+        <button class="search-button" @click="handleSearchClick">
+        🔍
+        </button>
+    </div>
+</template>
+<script></script>
